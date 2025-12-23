f8040f7788f3c3d35a006400353d4a5791da9a92
TheAustinMiller <themilleraustin@gmail.com>
2025-12-22 18:51:23 -0600

    Create working product model and product card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './core/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MockShop';
+  testProduct: Product = {
+    id: 1,
+    title: 'Test Shoes',
+    price: 45.99,
+    description: 'A very cool pair of mock shoes.',
+    category: 'footwear',
+    image: 'https://static.vecteezy.com/system/resources/previews/043/344/974/non_2x/high-performance-basketball-shoes-on-transparent-background-png.png',
+  };
 }
