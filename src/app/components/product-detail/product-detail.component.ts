36b1f5e197d8b34f5a4d651619316037e8461d2c
TheAustinMiller <themilleraustin@gmail.com>
2025-12-23 18:48:54 -0600

    Product detail page built out

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -9,25 +9,21 @@ import { ProductService } from 'src/app/core/product.service';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
-  // Inside product-detail.component.ts
   product: Product | undefined;
 
   constructor(
-    private route: ActivatedRoute,       // The "URL Listener"
-    private productService: ProductService // The "Data Fetcher"
+    private route: ActivatedRoute,
+    private productService: ProductService
   ) { }
 
   ngOnInit(): void {
-    // 1. Grab the ID from the URL snapshot
     const productId = this.route.snapshot.paramMap.get('id');
 
-    // 2. Convert it to a number (API usually expects numbers)
     if (productId) {
       const id = Number(productId);
 
-      // 3. Ask the service for the data
       this.productService.getProductById(id).subscribe(data => {
-        this.product = data; // Now you have the full product object!
+        this.product = data;
       });
     }
   }
