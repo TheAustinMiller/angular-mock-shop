497b0df84e5f59a0337c18e9a06ff6e63b42de20
TheAustinMiller <themilleraustin@gmail.com>
2025-12-24 18:39:44 -0600

    Build out the cart layout

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/core/cart.service';
+import { Product } from 'src/app/core/product.model';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+  count = 0;
+  cartCount$: Observable<number> | undefined;
+  items: Product[] = [];
 
+  constructor(private router: Router, private cartService: CartService) { this.cartCount$ = this.cartService.cartCount$; }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+
+    this.cartService.cartCount$.subscribe(val => {
+      this.count = val;
+    });
+  }
+
+  calculateTotal() {
+    return this.items.reduce((acc, item) => acc + item.price, 0);
+  }
 }
